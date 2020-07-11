e183cb12016d83cca727760f64762e9d376a41e6
Peter-Andonov <peter.g.andonov@gmail.com>
2020-07-11 16:27:15 +0300

    feat: added sidebar navigation container

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import IngredientComponentTest from './IngredientComponentTest'
+import Navbar from './Components/Navigation'
+import RecipeList from './Components/RecipeList'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div>
-        <IngredientComponentTest />
-      </div>
+      <Navbar />
+      <RecipeList />
     </div>
   );
 }
